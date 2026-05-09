8c2239fe066680900867884b967b09b239ee834d
ahadullabaig <ahadullabaig.16@gmail.com>
2026-05-09 22:21:21 +0530

    added question 1

---
+++ q1/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const followBtn = document.getElementById('followBtn');
+
+    followBtn.addEventListener('click', () => {
+        if (followBtn.innerText === "Follow") {
+            followBtn.innerText = "Following";
+            followBtn.classList.add('following');
+        } else {
+            followBtn.innerText = "Follow";
+            followBtn.classList.remove('following');
+        }
+    });
+});
