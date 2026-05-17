eb856fa9a093a85788d80e6e2e1ea20f8665158a
ahadullabaig <ahadullabaig.16@gmail.com>
2026-05-17 18:34:05 +0530

    modified Q4

--- q4/script.js
+++ q4/script.js
@@ -7,14 +7,12 @@ form.addEventListener('submit', (event) => {
     const name = document.getElementById('fullName').value;
     const email = document.getElementById('email').value;
     const course = document.getElementById('course').value;
-    const semester = document.querySelector('input[name="semester"]:checked').value;
 
     messageDiv.innerHTML = `
         <strong>Registration Successful!</strong><br>
         Student: ${name}<br>
         Email: ${email}<br>
         Course: ${course.toUpperCase()}<br>
-        Term: ${semester}
     `;
     messageDiv.classList.remove('hidden');
 });
